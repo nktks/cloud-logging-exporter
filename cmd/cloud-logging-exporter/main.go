0305533fa61d882c8e2864a6f29a03d9640baa71
takeshi.nakata <takeshi.nakata@mixi.co.jp>
2022-05-11 10:08:18 +0900

    refactor

--- cmd/cloud-logging-exporter/main.go
+++ cmd/cloud-logging-exporter/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -39,6 +40,10 @@ func init() {
 
 func main() {
 
+	if projectID == "" {
+		log.Fatal("need project flag.")
+	}
+
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 	client, err := logging.NewClient(ctx, projectID)
@@ -46,6 +51,54 @@ func main() {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 	defer client.Close()
+	exporter, err := newExporter(client, withStdoutLog, logName, severity)
+	if err != nil {
+		log.Fatalf("Failed to create exporter: %v", err)
+	}
+	var agent agent
+	if file == "" {
+		agent = newStdinAgent(exporter)
+	} else {
+		fp, err := os.Open(file)
+		newFile := false
+		if err != nil {
+			if os.IsNotExist(err) && touch {
+				fp, err = os.Create(file)
+				if err != nil {
+					log.Fatal(err)
+				}
+				newFile = true
+			} else {
+				log.Fatal(err)
+			}
+		}
+		defer fp.Close()
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer watcher.Close()
+		if err := watcher.Add(file); err != nil {
+			log.Fatal(err)
+		}
+
+		agent = newFileAgent(watcher, fp, exporter, exportSaved && !newFile)
+	}
+	go func() {
+		if err := agent.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	<-ctx.Done()
+	log.Println("completed")
+}
+
+type exporter struct {
+	withStdout bool
+	logger     *log.Logger
+}
+
+func newExporter(client *logging.Client, withStdout bool, logName, severity string) (*exporter, error) {
 	var lseverity logging.Severity
 	switch severity {
 	case "default":
@@ -67,82 +120,87 @@ func main() {
 	case "emergency":
 		lseverity = logging.Emergency
 	default:
-		log.Fatalf("unknown severity: %s", severity)
+		return nil, fmt.Errorf("unknown severity: %s", severity)
 	}
 	logger := client.Logger(logName).StandardLogger(lseverity)
+	return &exporter{
+		withStdout: withStdout,
+		logger:     logger,
+	}, nil
+}
 
-	if file == "" {
-		stdin := bufio.NewScanner(os.Stdin)
-		go func() {
-			for stdin.Scan() {
-				logger.Print(stdin.Text())
-				if withStdoutLog {
-					log.Print(stdin.Text())
-				}
+func (e *exporter) Export(s string) {
+	e.logger.Print(s)
+	if e.withStdout {
+		log.Print(s)
+	}
+}
+
+type agent interface {
+	Run() error
+}
+
+type fileAgent struct {
+	watcher     *fsnotify.Watcher
+	fp          *os.File
+	exporter    *exporter
+	exportSaved bool
+}
+
+func newFileAgent(watcher *fsnotify.Watcher, fp *os.File, exporter *exporter, exportSaved bool) agent {
+	return &fileAgent{
+		watcher:     watcher,
+		fp:          fp,
+		exporter:    exporter,
+		exportSaved: exportSaved,
+	}
+}
+
+func (a *fileAgent) Run() error {
+	b, err := io.ReadAll(a.fp)
+	if err != nil {
+		return err
+	}
+	if a.exportSaved {
+		a.exporter.Export(string(b))
+	}
+	for {
+		select {
+		case event, ok := <-a.watcher.Events:
+			if !ok {
+				return nil
 			}
-		}()
-	} else {
-		fp, err := os.Open(file)
-		newFile := false
-		if err != nil {
-			if os.IsNotExist(err) && touch {
-				fp, err = os.Create(file)
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				b, err := io.ReadAll(a.fp)
 				if err != nil {
-					log.Fatal(err)
+					return err
 				}
-				newFile = true
-			} else {
-				log.Fatal(err)
+				a.exporter.Export(string(b))
 			}
-		}
-		defer fp.Close()
-		b, err := io.ReadAll(fp)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if exportSaved && newFile {
-			logger.Print(string(b))
-			if withStdoutLog {
-				log.Print(string(b))
+		case err, ok := <-a.watcher.Errors:
+			if !ok {
+				return nil
 			}
-		}
-		watcher, err := fsnotify.NewWatcher()
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer watcher.Close()
-
-		go func() {
-			for {
-				select {
-				case event, ok := <-watcher.Events:
-					if !ok {
-						return
-					}
-					if event.Op&fsnotify.Write == fsnotify.Write {
-						bt, err := io.ReadAll(fp)
-						if err != nil {
-							log.Fatal(err)
-						}
-						logger.Print(string(bt))
-						if withStdoutLog {
-							log.Print(string(bt))
-						}
-					}
-				case err, ok := <-watcher.Errors:
-					if !ok {
-						return
-					}
-					log.Println("error:", err)
-				}
-			}
-		}()
-
-		err = watcher.Add(file)
-		if err != nil {
-			log.Fatal(err)
+			log.Println("error:", err)
 		}
 	}
-	<-ctx.Done()
-	log.Println("completed")
+	return nil
+}
+
+type stdinAgent struct {
+	scanner  *bufio.Scanner
+	exporter *exporter
+}
+
+func newStdinAgent(exporter *exporter) agent {
+	return &stdinAgent{
+		scanner:  bufio.NewScanner(os.Stdin),
+		exporter: exporter,
+	}
+}
+func (a *stdinAgent) Run() error {
+	for a.scanner.Scan() {
+		a.exporter.Export(a.scanner.Text())
+	}
+	return nil
 }
